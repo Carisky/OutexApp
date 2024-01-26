649a91f442c33234d24fdd7f56f29a1e8b4b659b
Yehor <egorkorsun7@gmail.com>
2024-01-26 04:50:56 +0100

    Workout Details Fetching

--- outexApp/pages/Workouts/WorkoutDetailsPage.js
+++ outexApp/pages/Workouts/WorkoutDetailsPage.js
@@ -1,18 +1,26 @@
 // Import necessary dependencies
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
 import GoBack from "../../components/GoBack";
+import APIhendler from "../../api/APIhendler";
 
 const WorkoutDetailsPage = ({ route, navigation }) => {
   // Extract the workoutId from the route params
   const { workoutId } = route.params;
-
+  const [workoutInfo,setWorkoutInfo] = useState()
+  useEffect( () => {
+    async function fetchData(){
+      const data = await APIhendler.getWorkoutWithExsercises(workoutId)
+      setWorkoutInfo(data)
+    }
+    fetchData()
+  }, []);
   return (
     <View>
       <GoBack navigation={navigation}/>
       <Text>Workout Details Page for Workout ID: {workoutId}</Text>
-      {/* Add your workout details rendering logic here */}
+      {workoutInfo && <Text>Info: {workoutInfo.info}</Text>}
     </View>
   );
 };
