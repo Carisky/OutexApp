40f31a2dea6f7fcad84a1b13c106a74c387b010b
Yehor <egorkorsun7@gmail.com>
2024-01-27 14:29:45 +0100

    remake component

--- outexApp/pages/Workouts/WorkoutDetailsPage.js
+++ outexApp/pages/Workouts/WorkoutDetailsPage.js
@@ -1,26 +1,34 @@
 // Import necessary dependencies
 import React, { useEffect, useState } from "react";
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, Card } from "react-native-paper";
 import GoBack from "../../components/GoBack";
 import APIhendler from "../../api/APIhendler";
 
 const WorkoutDetailsPage = ({ route, navigation }) => {
   // Extract the workoutId from the route params
   const { workoutId } = route.params;
-  const [workoutInfo,setWorkoutInfo] = useState()
-  useEffect( () => {
-    async function fetchData(){
-      const data = await APIhendler.getWorkoutWithExsercises(workoutId)
-      setWorkoutInfo(data)
+  const [workoutInfo, setWorkoutInfo] = useState();
+  useEffect(() => {
+    async function fetchData() {
+      const data = await APIhendler.getWorkoutWithExsercises(workoutId);
+      setWorkoutInfo(data);
     }
-    fetchData()
+    fetchData();
   }, []);
   return (
     <View>
-      <GoBack navigation={navigation}/>
-      <Text>Workout Details Page for Workout ID: {workoutId}</Text>
-      {workoutInfo && <Text>Info: {workoutInfo.info}</Text>}
+      <GoBack navigation={navigation} />
+      <Card mode='outlined' style={{
+        margin:"10px"
+      }}>
+        <Card.Cover source={{ uri: workoutInfo && workoutInfo.image_url }} />
+        <Card.Title title={workoutInfo && workoutInfo.description} />
+
+        <Card.Content>
+          <Text variant="bodyMedium">{workoutInfo && workoutInfo.info}</Text>
+        </Card.Content>
+      </Card>
     </View>
   );
 };
